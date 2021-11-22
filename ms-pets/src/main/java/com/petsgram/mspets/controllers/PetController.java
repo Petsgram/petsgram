5c3f01e29efa78301da56d3bbf518f3f96e0cfd5
criiscz <criissf15@gmail.com>
2021-11-22 18:34:41 -0500

    [PET-24] Moficar mascota creado

--- ms-pets/src/main/java/com/petsgram/mspets/controllers/PetController.java
+++ ms-pets/src/main/java/com/petsgram/mspets/controllers/PetController.java
@@ -3,9 +3,7 @@ package com.petsgram.mspets.controllers;
 import com.petsgram.mspets.models.Pet;
 import com.petsgram.mspets.repositories.PetRepository;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PetController {
@@ -20,4 +18,10 @@ public class PetController {
     Pet newPet(@RequestBody Pet pet) {
         return petRepository.save(pet);
     }
+
+    @PutMapping("/pets")
+    Pet updatePet(@RequestBody Pet pet) {
+        return petRepository.save(pet);
+    }
+
 }
