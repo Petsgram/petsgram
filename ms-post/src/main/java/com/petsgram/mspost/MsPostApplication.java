79c94df30e67d3385f72f4c91021e56017ddf255
criiscz <criissf15@gmail.com>
2021-11-26 09:42:09 -0500

    Created and initialized proyect spring

---
+++ ms-post/src/main/java/com/petsgram/mspost/MsPostApplication.java
@@ -0,0 +1,13 @@
+package com.petsgram.mspost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPostApplication.class, args);
+    }
+
+}
