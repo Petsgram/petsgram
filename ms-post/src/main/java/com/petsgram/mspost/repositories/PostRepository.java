b8c5447422158ff4f7717eef4edf56acabcf350c
criiscz <criissf15@gmail.com>
2021-12-01 21:44:36 -0500

    Some changes

--- ms-post/src/main/java/com/petsgram/mspost/repositories/PostRepository.java
+++ ms-post/src/main/java/com/petsgram/mspost/repositories/PostRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface PostRepository extends MongoRepository<Post, String> {
     Iterable<Post> findByOrderByDateDesc();
     List<Post> findByUsername(String username);
+    List<Post> findByUsernameOrderByDateDesc(String username);
 }
