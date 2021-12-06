935f7be83ac582f14a8cd573dde91d133d01ee9f
criiscz <criissf15@gmail.com>
2021-12-06 13:37:55 -0500

    prettier and eslint configured

--- gui/src/views/Profile.vue
+++ gui/src/views/Profile.vue
@@ -1,13 +1,9 @@
-<template>
-
-</template>
+<template></template>
 
 <script>
 export default {
-  name: "Profile"
-}
+  name: "Profile-View"
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
