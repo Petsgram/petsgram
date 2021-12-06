ab4f47d53f610c1d16d4d5497da68c2c9221fefa
criiscz <criissf15@gmail.com>
2021-12-06 09:50:53 -0500

    Uploaded mockups and created first documents

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,30 +1,11 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template id="app">
+  <nav>
+    <router-link to="Login">Login</router-link>
+    <router-link to="Register">Register</router-link>
+  </nav>
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ gui/src/views/Feed.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Feed"
+}
+</script>
+
+<style scoped>
+
+</style>
